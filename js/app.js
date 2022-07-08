aa2868613c88c2c43b6825570a87fdc4f94c356a
CIFU1199 <edwkamilo@gmail.com>
2022-07-08 15:07:17 -0500

    Primeros pasos

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const mascotaInput = document.querySelector('#mascota');
+const propietarioInput = document.querySelector('#propietario');
+const telefonoInput = document.querySelector('#telefono');
+const fechaInput = document.querySelector('#fecha');
+const horaInput = document.querySelector('#hora');
+const sintomasInput = document.querySelector('#sintomas');
+
+const formulario = document.querySelector('#nueva-cita');
